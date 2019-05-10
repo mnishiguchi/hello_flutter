4877c958ce1baed014cd9d340789550b0e2b7515
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2019-05-10 14:32:38 -0400

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+// A state that belongs to MyApp Widget.
+class _MyAppState extends State<MyApp> {
+  // properties
+  List<String> _products = ['Software Engineer'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text("Add Product"),
-              ),
+          appBar: AppBar(
+            title: Text("Hello Flutter"),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _products.add('Guitarist');
+                        });
+                    },
+                    child: Text("Add Product"),
+                  ),
+                ),
+                Column(
+                  children: _products
+                      .map(
+                        (text) => Card(
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/masa_chinatown.png'),
+                                  Text(text),
+                                ],
+                              ),
+                            ),
+                      )
+                      .toList(),
+                )
+              ],
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/masa_chinatown.png'),
-                  Text("Food Paradise"),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
