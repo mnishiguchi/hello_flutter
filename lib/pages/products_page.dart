a0feaf7079afdac34d21b9be5883b5428d6ddab0
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2019-05-11 16:14:10 -0400

    Drawer

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -6,8 +6,22 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
-        title: Text("Hello Flutter"),
+        title: Text('Hello Flutter'),
       ),
       body: Center(
         child: ProductManager(),
